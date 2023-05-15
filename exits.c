282ae0100c136103c5cf00abb35690d4c022c0a3
williamsudk <coolwillsxj@gmail.com>
2023-05-15 12:04:45 -0700

    C - Simple Shell

---
+++ exits.c
@@ -0,0 +1,74 @@
+#include "shell.h"
+
+/**
+ **_strncpy - Function that copies a string
+ *@dest: Parameter for the destination string to be copied to
+ *@src: Parameter for the source string
+ *@n: Parameter for the amount of characters to be copied
+ *Return: The concatenated string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	while (src[i] != '\0' && i < n - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i < n)
+	{
+		j = i;
+		while (j < n)
+		{
+			dest[j] = '\0';
+			j++;
+		}
+	}
+	return (s);
+}
+
+/**
+ **_strncat - Function that concatenates two strings
+ *@dest: Parameter for the first string
+ *@src: Parameter for the second string
+ *@n: Parameter for the amount of bytes to be maximally used
+ *Return: the concatenated string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	if (j < n)
+		dest[i] = '\0';
+	return (s);
+}
+
+/**
+ **_strchr - Function that locates a character in a string
+ *@s: Parameter for the string to be parsed
+ *@c: Parameter for the character to look for
+ *Return: (s) a pointer to the memory area s
+ */
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++ != '\0');
+
+	return (NULL);
+}
